9a4d46de015dfafa36a87e7ee9dcbb5da7983d97
Karel Suchomel <karelsuchomel@windowslive.com>
2018-12-05 11:20:44 +0100

    ADDED herocard portal-mockup mobile version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-console.log("printed from main.js");
+const portalMockupEl = document.getElementById('portal-mockup-graphic')
+const demoButtonEl = document.querySelector('#hero-card-content .segment-part button')
+
+const toggleMockUpGraphic = (e) => {
+	portalMockupEl.classList.toggle('focused')
+	demoButtonEl.classList.toggle('focused')
+}
+
+// Event listeners
+demoButtonEl.addEventListener('mouseenter', toggleMockUpGraphic)
+demoButtonEl.addEventListener('mouseleave', toggleMockUpGraphic)
+portalMockupEl.addEventListener('mouseenter', toggleMockUpGraphic)
+portalMockupEl.addEventListener('mouseleave', toggleMockUpGraphic)
