c0cdd6f552cf2f901992ba562cd6567ddc8d790b
Karel Suchomel <karelsuchomel@windowslive.com>
2019-01-18 18:43:10 +0100

    Additional touched to the examples segment, making it's compositin same in every screen width

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,25 +15,3 @@ demoButtonEl.addEventListener('mouseenter', toggleFocusedClassHeroCard)
 demoButtonEl.addEventListener('mouseleave', toggleFocusedClassHeroCard)
 portalMockupEl.addEventListener('mouseenter', toggleFocusedClassHeroCard)
 portalMockupEl.addEventListener('mouseleave', toggleFocusedClassHeroCard)
-
-//
-// Examples segment
-//
-
-const examplesDesktopPreview = document.querySelector('#desktop-preview-container img')
-const examplesMobilePreview = document.querySelector('#mobile-preview-container img')
-const examplesLink = document.querySelector('#examples-homepage-segment .content-wrap .project-details > p > a')
-
-const toggleFocusedClassExamples = (e) => {
-	examplesDesktopPreview.classList.toggle('focused')
-	examplesMobilePreview.classList.toggle('focused')
-	examplesLink.classList.toggle('focused')
-}
-
-// Event listeners
-examplesDesktopPreview.addEventListener('mouseenter', toggleFocusedClassExamples)
-examplesDesktopPreview.addEventListener('mouseleave', toggleFocusedClassExamples)
-examplesMobilePreview.addEventListener('mouseenter', toggleFocusedClassExamples)
-examplesMobilePreview.addEventListener('mouseleave', toggleFocusedClassExamples)
-examplesLink.addEventListener('mouseenter', toggleFocusedClassExamples)
-examplesLink.addEventListener('mouseleave', toggleFocusedClassExamples)
